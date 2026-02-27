552372aab5302011f2e26640a24b3bd056321ec7
knara7823 <knaraharutyunova@gmail.com>
2026-02-27 11:03:30 +0400

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ int main(){
     int n = 5;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++)
-            cout << "!mnnmnm hhh";
+            cout << "!mnnmdfdfdfnm hhh";
         cout << endl;
     }
     return 0;
